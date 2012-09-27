8731393e6caa8acf75ee691f984239b7f2fc0e81
Nikolay Filchenko <finomen812@gmail.com>
2012-09-27 23:47:58 +0400

    Beginning of network part

--- src/pcms2/timer/TimerNotifier.java
+++ src/pcms2/timer/TimerNotifier.java
@@ -12,7 +12,9 @@ public class TimerNotifier extends BaseComponent implements ITimerNotifier {
 	private IThread worker;
 	private ClockService cs;
 	private String clockId;
-	public void activate() {
+	
+		
+	public void activate() throws ConfigurationException {
 		registerService(null, ITimerNotifier.class, this);
 		log.info("Timer notifier service registered");
 		try {
@@ -21,11 +23,47 @@ public class TimerNotifier extends BaseComponent implements ITimerNotifier {
 			e.printStackTrace();
 		}
 		
-		try {
-			clockId = config.getString("clock-id");
-		} catch (ConfigurationException e) {
-			e.printStackTrace();
+		clockId = config.getString("clock-id");
+				
+		if (config.hasAttribute("multicast-group")) {
+			log.info("Initiating multicast notifier");
+			if (config.hasAttribute("multicast-port")) {
+				int port = config.getInt("multicast-port");
+				String group = config.getString("multicast-group");
+				
+				log.info("Multicast notifier initiated");
+			} else {
+				log.error("Required param multicast-port is not set. Multicast notifier disabled");
+			}
+			
 		}
+		
+		if (config.hasAttribute("broadcast-addr")) {
+			log.warning("Broadcast is very bad idea");
+			log.info("Initiating broadcast notifier");
+			if (config.hasAttribute("broadcast-port")) {
+				int port = config.getInt("broadcast-port");
+				String addr = config.getString("broadcast-addr");
+				
+				log.info("Broadcast notifier initiated");
+			} else {
+				log.error("Required param broadcast-port is not set. Broadcast notifier disabled");
+			}
+		}
+		
+		if (config.hasAttribute("udp-port")) {
+			log.info("Initiating UDP notifier");
+			int port = config.getInt("udp-port");
+			log.info("UDP notifier initiated");
+		}
+		
+		if (config.hasAttribute("tcp-port")) {
+			log.info("Initiating TCP notifier");
+			int port = config.getInt("tcp-port");
+			log.info("TCP notifier initiated");
+		}
+		
+		
 		worker = startThread(this, "timer-notifier-thread");
 	}
 	
